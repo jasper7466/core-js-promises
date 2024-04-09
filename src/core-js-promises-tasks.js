2685a9cbc27e114e162108ab5806ea642d68e9cb
Tim <jasper7466@yandex.ru>
2024-04-09 19:19:41 +0500

    feat: solve "getFirstResolvedPromiseResult" problem

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -58,8 +58,14 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.allSettled(promises).then((settledPromises) => {
+    const firstSettled = settledPromises.find(
+      (promise) => promise.status === 'fulfilled'
+    );
+
+    return firstSettled ? firstSettled.value : Promise.reject();
+  });
 }
 
 /**
